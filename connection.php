ef755c337c17f0c5c37eb43f353a028f7cfb6b32
Benson Macharia <muchokibenson12@gmail.com>
2021-06-21 19:51:37 +0300

    Added Database File

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = mysqli_connect('localhost', 'bmacharia', '12qwaszxasqw12', 'edms');
+$connection = mysqli_connect('localhost', 'user', 'password', 'database');
 if (mysqli_connect_error()) {
     echo "failed to connect:" . mysqli_connect_error();
 }
